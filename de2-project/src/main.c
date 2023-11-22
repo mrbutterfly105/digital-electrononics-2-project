fddbcd73a3e89c29a9656d48ac18906b1aefd63e
mrbutterfly105 <102173814+mrbutterfly105@users.noreply.github.com>
2023-11-22 13:48:37 +0100

    vytvoření základních proměnných
    Je potřeba dodělat funkce pro init

---
+++ de2-project/src/main.c
@@ -0,0 +1,28 @@
+
+// includes
+
+// end includes
+
+int main (void){
+
+// inicializace pinů a globálních proměnných
+int soil_humidity = get_soil_humidytiy();
+int air_humidity = get_air_humidity();
+int air_temp = get_air_temp();
+int tank_level = get_tank_level(); // in %
+int room_light = get_room_light(); 
+int lamp_intenzity = 0;
+bool pump_state = false;
+bool low_water_level = false; 
+bool bad_temp = false;
+
+
+while(1){
+// tady bude všechna magie
+
+  ;
+}
+
+
+return 0;
+}
