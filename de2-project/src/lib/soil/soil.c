21a0687dc9608b7232d3b15615ac9f21428532ab
husslt <124718925+husslt@users.noreply.github.com>
2023-12-06 12:31:57 +0100

    Soil branch with LCD test

---
+++ de2-project/src/lib/soil/soil.c
@@ -0,0 +1,53 @@
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <soil.h>
+#include <gpio.h>
+
+uint16_t getSoilHumidityPercentage() {
+    volatile uint16_t ADCvalue;          // value returned by ADC
+    float percent;              // percentage calculation handling
+    uint8_t pct;                // return value
+    uint16_t max = 512;         // 0-1024 ... 0-5V, set maximum level of ADC, which will equal to 100 %, e.g. max voltage is 2,5V, then value is 2,5/5 * 1024 = 
+    uint16_t min = 246;         // 0-1024, set minimum voltage corresponding to 0 %
+    ADMUX &= ~(1<<REFS0); ADMUX |= (1<<REFS0); // Set reference Vref to AVCC, REFS1 = 0, REFS0 = 1
+    ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0)); // Set ADC input source to ADC0, MUX3:0 = 0000
+    ADMUX &= ~(1<<ADLAR);       // Set result register to be right adjusted, ADLAR = 0
+    ADCSRA |= (1<<ADEN);        // Enable ADC conversion, ADEN = 1
+    //ADCSRA |= (1<<ADIE);      // Enable ADC interrupt, ADIE = 1
+    ADCSRA |= ((1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0)); // Set frequency division factor to 128, Arduino Uno clock = 16 MHz, ADC clock has to be between 50 and 200 kHz
+    ADCSRA |= (1<<ADSC);        // Start ADC conversion
+    while ((ADCSRA&(1<<ADIF)) == 0) {;}; // wait for conversion to finish (polling method)
+    ADCvalue = ADC;             // ADC finished, put result variable into ADCvalue
+    ADCSRA &= ~(1<<ADIF);       // clear ADIF after conversion (ADIF is not cleared automatically unless we are using interrupt method)
+    percent = (float)(ADCvalue-min)/(max-min)*100; // calculate percentage, normalized to the interval defined by max and min variables
+    pct = (int)percent;         // convert float back to int (with a small flooring error)
+    return pct;
+}
+
+uint16_t getTankLevelPercentage() {
+    uint16_t ADCvalue;          // value returned by ADC
+    float percent;              // percentage calculation handling
+    uint8_t pct;                // return value
+    uint16_t max = 512;         // 0-1024 ... 0-5V, set maximum level of ADC, which will equal to 100 %, e.g. max voltage is 2,5V, then value is 2,5/5 * 1024 = 
+    uint16_t min = 246;         // 0-1024, set minimum voltage corresponding to 0 %
+    ADMUX &= ~(1<<REFS0); ADMUX |= (1<<REFS0); // Set reference Vref to AVCC, REFS1 = 0, REFS0 = 1
+    ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1)); ADMUX |= (1<<MUX0); // Set ADC input source to ADC1, MUX3:0 = 0001
+    ADMUX &= ~(1<<ADLAR);       // Set result register to be right adjusted, ADLAR = 0
+    ADCSRA |= (1<<ADEN);        // Enable ADC conversion, ADEN = 1
+    //ADCSRA |= (1<<ADIE);      // Enable ADC interrupt, ADIE = 1
+    ADCSRA |= ((1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0)); // Set frequency division factor to 128, Arduino Uno clock = 16 MHz, ADC clock has to be between 50 and 200 kHz
+    ADCSRA |= (1<<ADSC);        // Start ADC conversion
+    while ((ADCSRA&(1<<ADIF)) == 0){;}; // wait for conversion to finish (polling method)
+    ADCvalue = ADC;             // ADC finished, put result variable into ADCvalue
+    ADCSRA &= ~(1<<ADIF);       // clear ADIF after conversion (ADIF is not cleared automatically unless we are using interrupt method)
+    percent = (float)(ADCvalue-min)/(max-min)*100; // calculate percentage, normalized to the interval defined by max and min variables
+    pct = (int)percent;         // convert float back to int (with a small flooring error)
+    return pct;
+}
+
+void pumpTurnOff() {
+    GPIO_write_low(&DDRB, PB0);
+}
+
+void pumpTurnOn() {
+    GPIO_write_high(&DDRB, PB0);
+}
--- de2-project/src/main.c
+++ de2-project/src/main.c
@@ -1,28 +1,74 @@
-
-// includes
-
-// end includes
-
-int main (void){
-
-// inicializace pinů a globálních proměnných
-int soil_humidity = get_soil_humidytiy();
-int air_humidity = get_air_humidity();
-int air_temp = get_air_temp();
-int tank_level = get_tank_level(); // in %
-int room_light = get_room_light(); 
-int lamp_intenzity = 0;
-bool pump_state = false;
-bool low_water_level = false; 
-bool bad_temp = false;
+/***********************************************************************
+ * 
+ * Use Analog-to-digital conversion to read push buttons on LCD keypad
+ * shield and display it on LCD screen.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2018 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
 
 
-while(1){
-// tady bude všechna magie
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <lcd.h>
+#include <stdlib.h>         // C library. Needed for number conversions
+#include <soil.h>  
+#include <stdio.h>
 
-  ;
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Use Timer/Counter1 and start ADC conversion every 100 ms.
+ *           When AD conversion ends, send converted value to LCD screen.
+ * Returns:  none
+ **********************************************************************/
+
+//uint16_t ADCvalue; // variable to be used by interrupt routine
+
+
+int main(void)
+{
+    lcd_init(LCD_DISP_ON);
+    lcd_gotoxy(0,0);
+    lcd_puts("SOIL: ");
+    lcd_gotoxy(0,1);
+    lcd_puts("TANK: ");
+
+    TIM1_OVF_1SEC
+    TIM1_OVF_ENABLE
+    
+    sei();
+
+    while (1) {
+        ;
+    }
+
+    return 0;
 }
 
 
-return 0;
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Use single conversion mode and start conversion every 100 ms.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    uint8_t soil = getSoilHumidityPercentage();
+    uint8_t tank = getTankLevelPercentage();
+    char string[3];
+    itoa(soil, string, 10);
+    lcd_gotoxy(6,0);
+    lcd_puts(string);
+    itoa(tank, string, 10);
+    lcd_gotoxy(6,1);
+    lcd_puts(string);
 }
+
+
