ce5b9ca36b997db8fbd75899a76611fb0a99f1d6
grizzly42 <61315339+grizzly42@users.noreply.github.com>
2023-12-12 17:29:31 +0100

    Update buttons.c

--- de2-project/lib/buttons/buttons.c
+++ de2-project/lib/buttons/buttons.c
@@ -1,3 +1,7 @@
+/**
+ * @file buttons.c
+ * @brief Button handling functions implementation file.
+ */
 #include <gpio.h>
 #include <uart.h>
 
@@ -5,7 +9,9 @@
 #define LEFT PD2
 #define ENTER PD3
 
-
+/**
+ * @brief Initializes the buttons by setting the corresponding pins as input with pull-up.
+ */
 
 void init_buttons()
 {
@@ -16,6 +22,11 @@ void init_buttons()
 }
 
 // LEFT, RIGHT, ENTER
+/**
+ * @brief Gets the state of the specified button.
+ * @param button The button to check (LEFT, RIGHT, or ENTER).
+ * @return The state of the button (1 for pressed, 0 for not pressed).
+ */
 uint8_t get_button_state(uint8_t button){
     return GPIO_read(&PIND, button);
 }
