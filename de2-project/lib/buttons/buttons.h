bc6c81d712121f86391e236af2422b8676deea0c
grizzly42 <61315339+grizzly42@users.noreply.github.com>
2023-12-12 17:28:32 +0100

    Update buttons.h

--- de2-project/lib/buttons/buttons.h
+++ de2-project/lib/buttons/buttons.h
@@ -1,4 +1,8 @@
 // gpio_switch.h
+/**
+ * @file buttons.h
+ * @brief Button handling functions header file.
+ */
 
 #ifndef GPIO_SWITCH_H
 #define GPIO_SWITCH_H
@@ -10,7 +14,15 @@
 #include <stdint.h>
 
 // Deklarace funkce pro čtení stavu tlačítka
+/**
+ * @brief Initializes the buttons by setting the corresponding pins as input with pull-up.
+ */
 void init_buttons();
+/**
+ * @brief Gets the state of the specified button.
+ * @param button The button to check (LEFT, RIGHT, or ENTER).
+ * @return The state of the button (1 for pressed, 0 for not pressed).
+ */
 uint8_t get_button_state(uint8_t button);
 
 #endif // GPIO_SWITCH_H
